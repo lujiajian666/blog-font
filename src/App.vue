8dbc0a07360876f1d6f834b190c0d09771629cc9
lujiajian <lujj@corp.21cn.com>
2019-02-15 00:06:50 +0800

    feat:新增article页面样式

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,17 @@
   text-align: center;
   color: #2c3e50;
 }
+
+// 自定义富文本样式
+  .ql-syntax {
+    background: black;
+    border-radius: 5px;
+    padding: 5px;
+  }
+  blockquote {
+    border-left: 4px solid #ccc;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    padding-left: 16px;
+  }
 </style>
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,40 +1,96 @@
 <template>
   <div class="index" v-if="tableData">
-    <h1>{{tableData.title}}</h1>
-    <div v-html="tableData.text"></div>
+    <p class="tag">lulujiang的个人博客</p>
+    <div class="container">
+      <div class="inner">
+        <h1>{{tableData.title}}</h1>
+        <p class="time">{{tableData.createTime}}</p>
+        <div v-html="tableData.text"></div>
+      </div>
+    </div>
+    <div class="container-mirror"></div>
   </div>
 </template>
 
 <script>
-import { get } from '../service/axios'
+  import {
+    get
+  } from '../service/axios'
   export default {
-    data () {
+    data() {
       return {
         tableData: null
       }
     },
-    created () {
-       get('/article/get', {
-          id: this.$route.params.id
-        }).then(res => {
-          this.tableData = res.data;
-        })
+    created() {
+      get('/article/get', {
+        id: this.$route.params.id
+      }).then(res => {
+        this.tableData = res.data;
+      })
     },
     methods: {
-    
+
     }
   }
+
 </script>
 
 <style lang="less" scoped>
-   .index {
-     ul {
-       text-align: center;
-       li {
-         font-size: 18px;
-         list-style: none;
-         cursor: pointer;
-       }
-     }
-   }
+  .index {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: calc(100% - 80px);
+    width: calc(100% - 80px);
+    padding: 40px;
+    text-align: left;
+    background: url('../assets/bg2.png') no-repeat center center;
+    background-size: 100% 100%;
+    overflow: hidden;
+
+    .tag {
+      height: 40px;
+      color: orange;
+    }
+
+    .container {
+      position: relative;
+      z-index: 1;
+      height: 90%;
+      margin: 10px auto;
+      overflow: auto;
+
+      .time {
+        margin-bottom: 40px;
+        color: #d3d3d3;
+        border-bottom: solid #d3d3d3 1px;
+      }
+
+      .inner {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        color: white;
+        box-sizing: border-box;
+      }
+    }
+
+    .container-mirror {
+        position: absolute;
+        top: 90px;
+        left: 40px;
+        right: 40px;
+        bottom: 40px;
+        background: url('../assets/bg2.png') no-repeat center center fixed;
+        background-size: 100% 100%;
+        filter: blur(10px);
+        opacity: .9;
+    }
+
+  }
+
+  
 </style>
