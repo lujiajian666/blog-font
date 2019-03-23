2c21a93c2e85059ec97bbfceef75718bc1e61005
lujiajian <lujj@corp.21cn.com>
2019-03-23 21:53:43 +0800

    refactor:更改样式

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,51 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-* {
-  padding: 0;
-  margin: 0
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    padding: 0;
+    margin: 0
+  }
 
-// 自定义富文本样式
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  // 自定义富文本样式
   .ql-syntax {
     background: black;
     border-radius: 5px;
     padding: 5px;
   }
+
   blockquote {
     border-left: 4px solid #ccc;
     margin-bottom: 5px;
     margin-top: 5px;
     padding-left: 16px;
   }
+
   .ql-align-center {
     text-align: center;
   }
+
   .index img {
     max-width: 100%;
   }
+
+  pre {
+    white-space: pre-wrap;
+    white-space: -moz-pre-wrap;
+    white-space: -pre-wrap;
+    white-space: -o-pre-wrap;
+    word-wrap: break-word;
+  }
+
 </style>
